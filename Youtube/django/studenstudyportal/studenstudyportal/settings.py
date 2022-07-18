57fcf88d57a91d9a94fe7baebe2670cd6ce4ba57
penscola <67982554+penscola@users.noreply.github.com>
2022-07-19 01:36:21 +0300

    changes made

--- Youtube/django/studenstudyportal/studenstudyportal/settings.py
+++ Youtube/django/studenstudyportal/studenstudyportal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uk#yknujjykkmp7g2wr*4my!$3ftqw+^__z9yplq1njyf0ahm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','student-study-portal254.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
